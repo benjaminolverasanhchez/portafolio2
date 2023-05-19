61f96c97c35d8af00ab2f146af8ff178c1f67025
benjaminolverasanhchez <125666998+benjaminolverasanhchez@users.noreply.github.com>
2023-05-19 07:53:09 -0600

    Add files via upload

---
+++ EPDescuento.js
@@ -0,0 +1,11 @@
+function descuento(){
+    var nombres = document.getElementById("nombre").value;
+    var fechas = document.getElementById("fecha").value;
+
+    var resultado4=fechas.getMonth(4)
+
+    if(resultado4=true){
+        document.getElementById("comentario").innerHTML="TU DESCUENTO ES DEL:";
+        }
+    }
+    
